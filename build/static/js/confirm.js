ce8633a70986ffb509e956c3716cb49f1b4e418f
Иван Тимошенко <vanya.timoschenko85@gmail.com>
2020-02-17 12:25:18 +0200

    1,5 tasks

--- build/static/js/confirm.js
+++ build/static/js/confirm.js
@@ -1,5 +1,5 @@
 let submit = document.getElementById("btn-subm");
-let name = document.getElementById("name");
+// let name = document.getElementById("name");
 let phone = document.getElementById("phone");
 let error = document.getElementById("error");
 let error_text = document.getElementById("error_text");
@@ -7,14 +7,23 @@ let form = document.getElementById("form");
 let conf = document.getElementById("conf");
 
 
-const name_regexp = new RegExp("[A-Za-zА-Яа-яЁё]{2,}");
+// const name_regexp = new RegExp("[A-Za-zА-Яа-яЁё]{2,}");
 // const phone_regexp = /(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?/;
 const phone_regexp = /[0-9]{10}/;
 const sms_regexp = /^\d{4}$/;
 
+phone.addEventListener("keydown", function() {
+    let value = this.value; 
+    let rep = /[-\.;":'a-zA-Zа-яА-Я]/; 
+    if (rep.test(value)) { 
+        value = value.replace(rep, ''); 
+        this.value = value;
+    } 
+});
+
 submit.addEventListener("click", (e) => {
     e.preventDefault();
-    if(!name.value){
+    /*if(!name.value){
         error.style.display = "block";
         error.style.top = '50px';
         error_text.innerHTML = "Значение не должно быть пустым";
@@ -24,14 +33,16 @@ submit.addEventListener("click", (e) => {
         error.style.top = '50px';
         error_text.innerHTML = "Имя должно состоять только из буков и не менее 2х";
         setTimeout(() => error.style.display = "none", 3000);
-    }else if(!phone.value){
+    }else*/
+    
+    if(!phone.value){
         error.style.display = "block";
-        error.style.top = '135px';
+        error.style.top = '60px';
         error_text.innerHTML = "Значение не должно быть пустым";
         setTimeout(() => error.style.display = "none", 3000);
     }else if(!phone_regexp.test(phone.value)){
         error.style.display = "block";
-        error.style.top = '135px';
+        error.style.top = '60px';
         error_text.innerHTML = "Введите правильный номер";
         setTimeout(() => error.style.display = "none", 3000);
     }else{
@@ -39,11 +50,11 @@ submit.addEventListener("click", (e) => {
         let sms = document.getElementById("conf-val");
         if(!sms.value){
             error.style.display = "block";
-            error.style.top = '265px';
+            error.style.top = '200px';
             error_text.innerHTML = "Введите код из смс";
         }else if(!sms_regexp.test(sms.value)){
             error.style.display = "block";
-            error.style.top = '265px';
+            error.style.top = '200px';
             error_text.innerHTML = "Введите код в правильном формате";
             setTimeout(() => error.style.display = "none", 3000);
         }else{
